c7af33bfc419996a6853eafe756308c3a950df62
Zig Zichterman <ziggr@ziggr.com>
2017-05-07 15:04:06 -0700

    Quest status starting to work, display

--- ZZDailyWrits.lua
+++ ZZDailyWrits.lua
@@ -13,12 +13,12 @@ DW.default         = {
 -- Use these values for craft_type
 --
 DW.CRAFTING_TYPE = {
-  { abbr = "bs", ct = CRAFTING_TYPE_BLACKSMITHING }
-, { abbr = "cl", ct = CRAFTING_TYPE_CLOTHIER      }
-, { abbr = "ww", ct = CRAFTING_TYPE_WOODWORKING   }
-, { abbr = "al", ct = CRAFTING_TYPE_ALCHEMY       }
-, { abbr = "en", ct = CRAFTING_TYPE_ENCHANTING    }
-, { abbr = "pr", ct = CRAFTING_TYPE_PROVISIONING  }
+  { abbr = "bs", order = 1, ct = CRAFTING_TYPE_BLACKSMITHING , quest_name = "Blacksmithing Writ"}
+, { abbr = "cl", order = 2, ct = CRAFTING_TYPE_CLOTHIER      , quest_name = "Clothier Writ" }
+, { abbr = "ww", order = 3, ct = CRAFTING_TYPE_WOODWORKING   , quest_name = "Woodworking Writ"}
+, { abbr = "al", order = 4, ct = CRAFTING_TYPE_ALCHEMY       , quest_name = "Alchemy Writ"}
+, { abbr = "en", order = 5, ct = CRAFTING_TYPE_ENCHANTING    , quest_name = "Enchanter Writ"}
+, { abbr = "pr", order = 6, ct = CRAFTING_TYPE_PROVISIONING  , quest_name = "Provisioner Writ"}
 }
 
 local DWUI = nil
@@ -29,23 +29,31 @@ local DWUI = nil
                         -- Not started
                         -- Need to visit a billboard to acquire quest.
                         -- "X" icon.
-DW.STATE_0_NEEDS_ACQUIRE    = "X"
+DW.STATE_0_NEEDS_ACQUIRE    = { id = "acqr", order = 0 }
 
                         -- Acquired, but at least one item needs to be
                         -- crafted before turnining in.
                         -- Need to visit a crafting station to
                         -- make things.
                         -- "Anvil" icon.
-DW.STATE_1_NEEDS_CRAFTING   = "A"
+DW.STATE_1_NEEDS_CRAFTING   = { id = "crft", order = 1 }
 
                         -- Crafting of all items completed.
                         -- Need to visit turn-in station.
                         -- "Bag" icon
-DW.STATE_2_NEEDS_TURN_IN    = "B"
+DW.STATE_2_NEEDS_TURN_IN    = { id = "turn", order = 2 }
 
                         -- Quest completed. Done for the day.
                         -- "Checkmark" icon.
-DW.STATE_3_TURNED_IN        = "V"
+DW.STATE_3_TURNED_IN        = { id = "done", order = 3 }
+
+function DW.StateMax(a, b)
+    if a.order < b.order then
+        return b
+    else
+        return a
+    end
+end
 
 -- QuestStatus --------------------------------------------------------------
 --
@@ -54,7 +62,8 @@ DW.STATE_3_TURNED_IN        = "V"
 DW.QuestStatus = {
 }
 function DW.QuestStatus:New()
-    local o = { status      = DW.STATE_0_NEEDS_ACQUIRE
+    local o = { state       = DW.STATE_0_NEEDS_ACQUIRE
+              , text        = ""
               , acquired_ts = 0  -- When we received the event for "acquired
                                  -- this quest," or 11pm yesterday if we
                                  -- missed that event and we're learning of
@@ -69,8 +78,8 @@ end
 --
 -- Current character's crafting quests data.
 --
--- Knows how to query current (aka "journal") quests to find any current quest status.
--- Knows to remember up to two quests' states, resets
+-- Knows how to query current (aka "journal") quests to find any
+-- current quest status.
 
 local CharData = {
 }
@@ -86,6 +95,145 @@ function CharData:New()
     return o
 end
 
+-- Scan the "Quest Journal" for active crafting quests.
+function CharData:ScanJournal()
+    local quest_ct = GetNumJournalQuests()
+    local quest_status = {}
+    for qi = 1, quest_ct do
+        local x = self:AbsorbQuest(qi)
+        if x then
+            quest_status[x.crafting_type.order] = x.quest_status
+d("Set qs["..tostring(x.crafting_type.order).."]")
+d(x)
+        end
+    end
+
+                        -- XXX Merge with previous quest_status list here
+                        -- XXX to retain previously turned-in
+                        -- XXX quests.
+
+    self.quest_status = quest_status
+d("char_data.quest_status:  ct="..#self.quest_status)
+d(self.quest_status[2])
+end
+
+-- GetJournalQuestInfo() returns:
+DW.JQI = {
+  quest_name            =  1 -- string
+, background_text       =  2 -- string
+, active_step_text      =  3 -- string
+, active_step_type      =  4 -- number
+, active_step_tracker_override_text =  5 -- string
+, completed             =  6 -- boolean
+, tracked               =  7 -- boolean
+, quest_level           =  8 -- number
+, pushed                =  9 -- boolean
+, quest_type            = 10 -- number
+, instance_display_type = 11 -- number InstanceDisplayType
+}
+
+-- GetJournalQuestStepInfo() returns
+DW.JQSI = {
+  step_text             = 1 -- string
+, visibility            = 2 -- number:nilable
+, step_type             = 3 -- number
+, tracker_override_text = 4 -- string
+, num_conditions        = 5 -- number
+}
+
+-- GetJournalQuestConditionInfo() returns
+DW.JQCI = {
+  condition_text        = 1 -- string
+, current               = 2 -- number
+, max                   = 3 -- number
+, is_fail_condition     = 4 -- boolean
+, is_complete           = 5 -- boolean
+, is_credit_shared      = 6 -- boolean
+, is_visible            = 7 -- boolean
+}
+
+-- Fetch a quest's data from server. If it's a crafting quest,
+-- store its quest_status in the appropriate self.quest_status[] slot.
+function CharData:AbsorbQuest(quest_index)
+                        -- Only daily quests matter.
+    local rt = GetJournalQuestRepeatType(quest_index)
+    if rt ~= QUEST_REPEAT_DAILY then
+        -- d(tostring(quest_index).." not daily "..tostring(rt))
+        return
+    end
+                        -- Only daily CRAFTING quests matter.
+    local qinfo = { GetJournalQuestInfo(quest_index) }
+    if qinfo[DW.JQI.quest_type] ~= QUEST_TYPE_CRAFTING then
+        -- d(tostring(quest_index).." not crafting "..tostring(qinfo[DW.JQI.quest_type]))
+        return
+    end
+                        -- Find correct index into CharData.quest_status[]
+    local crafting_type = self:QuestNameToCraftingType(qinfo[DW.JQI.quest_name])
+    if not crafting_type then
+        d("quest_name matches no index: "..tostring(qinfo[DW.JQI.quest_name]))
+        return
+    end
+
+                        -- Accumulate conditions into a quest_status.
+    local quest_status = self:AccumulateCondition(quest_index)
+
+    return { crafting_type = crafting_type
+           , quest_status  = quest_status
+           }
+end
+
+function CharData:QuestNameToCraftingType(quest_name)
+    for i, ct in ipairs(DW.CRAFTING_TYPE) do
+        if quest_name:find(ct.quest_name) then
+            return ct
+        end
+    end
+    return nil
+end
+
+-- Return a quest's conditions as a single QuestStatus instance.
+function CharData:AccumulateCondition(quest_index)
+    local text_list = {}
+    local status    = DW.STATE_1_NEEDS_CRAFTING
+
+                        -- Accumulate quest's current status, which always seems to
+                        -- be the status of the last/highest-indexed step.
+    local step_ct = GetJournalQuestNumSteps(quest_index)
+    local step_index = step_ct
+    local sinfo = { GetJournalQuestStepInfo(quest_index, step_index) }
+-- d(sinfo)
+    local condition_ct = sinfo[DW.JQSI.num_conditions]
+    for ci = 1, condition_ct do
+        local cinfo = { GetJournalQuestConditionInfo(quest_index, step_index, ci) }
+-- d("Condition " .. tostring(ci))
+-- d(cinfo)
+                        -- If we have not completed all the required counts
+                        -- for this condition, then its text matters.
+        if cinfo[DW.JQCI.is_visible]
+                and ( cinfo[DW.JQCI.current] < cinfo[DW.JQCI.max] ) then
+                local c_text = cinfo[DW.JQCI.condition_text]
+            table.insert(text_list, c_text)
+            status = DW.StateMax(status, self:ConditionTextToState(c_text))
+        end
+    end
+
+    local quest_status = DW.QuestStatus:New()
+    quest_status.status = status
+    quest_status.text   = table.concat(text_list, "\n")
+    return quest_status
+end
+
+-- If a crafting quest exists in the journal, it either needs to be
+-- crafted, or needs to be turned in.
+function CharData:ConditionTextToState(condition_text)
+    local says_deliver = string.find(condition_text, "Deliver ")
+    if says_deliver then
+        return DW.STATE_2_NEEDS_TURN_IN
+    else
+        return DW.STATE_1_NEEDS_CRAFTING
+    end
+end
+
 -- Init ----------------------------------------------------------------------
 
 function DW.OnAddOnLoaded(event, addonName)
@@ -99,6 +247,8 @@ function DW:Initialize()
     if not DWUI then
         DWUI = ZZDailyWritsUI
     end
+
+    DW.char_data = CharData:New()
 
     ZO_CreateStringId("SI_BINDING_NAME_ZZDW_TOGGLE_VISIBILITY", "Show/Hide")
 
@@ -118,7 +268,7 @@ function DW:RestorePos()
     pos = self.savedVariables.position
     if not pos then
         pos = self.default.position
-        d("pos huh?")
+        -- d("pos huh?")
     end
 
     DWUI:SetAnchor(
@@ -131,7 +281,7 @@ function DW:RestorePos()
 end
 
 function DW:SavePos()
-    d("SavePos")
+    --d("SavePos")
     self.savedVariables.position = { DWUI:GetLeft()
                                    , DWUI:GetTop()
                                    }
@@ -140,7 +290,7 @@ end
 -- Keybinding ----------------------------------------------------------------
 
 function ZZDailyWrits.ToggleVisibility()
-    d("ZZDW.ToggleVisibility")
+    -- d("ZZDW.ToggleVisibility")
     ui = DWUI
     if not ui then
         d("No UI")
@@ -149,6 +299,8 @@ function ZZDailyWrits.ToggleVisibility()
     local h = DWUI:IsHidden()
     if h then
         DW:RestorePos()
+        DW.char_data:ScanJournal()
+        DW:DisplayCharData()
     end
     DWUI:SetHidden(not h)
 end
@@ -164,6 +316,18 @@ function DW_Initialized()
 
 end
 --]]
+
+function DW:DisplayCharData()
+    for _,ct in ipairs(DW.CRAFTING_TYPE) do
+        local quest_status = self.char_data.quest_status[ct.order]
+        if quest_status then
+            local ui = ZZDailyWritsUI:GetNamedChild("_status_"..ct.abbr)
+            ui:SetText(quest_status.status.id)
+d("UI set "..ct.abbr)
+        end
+
+    end
+end
 
 -- Postamble -----------------------------------------------------------------
 
