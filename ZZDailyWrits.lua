8d3cac4b3ab65440e213ee04996c8dbd7fe39937
Zig Zichterman <ziggr@ziggr.com>
2017-05-07 11:04:23 -0700

    Archive ambitious plan
    About to scale back, drastically.
    
    Mothballing idea of tracking and displaying multiple days' crafting.
    Would be cool to know "you have N days worth of inventory before
    you need to visit a crafting station" but that makes things far too
    complicated and brittle and unlikely to ever be completed.
    
    Simplify. Just track CURRENT writ quest status.

--- ZZDailyWrits.lua
+++ ZZDailyWrits.lua
@@ -9,77 +9,62 @@ DW.default         = {
     position = {350,100}
 }
 
--- Quest states --------------------------------------------------------------
 
-                        -- Not started
-                        -- Need to visit a billboard to acquire quest.
-                        -- "X" icon.
-DW.STATE_0_NEEDS_ACQUIRE    = "X"
-
-                        -- Acquired, but at least one item needs to be
-                        -- crafted before turnining in.
-                        -- Need to visit a crafting station to
-                        -- make things.
-                        -- "Anvil" icon.
-DW.STATE_1_NEEDS_CRAFTING   = "A"
-
-                        -- Crafting of all items completed.
-                        -- Need to visit turn-in station.
-                        -- "Bag" icon
-DW.STATE_2_NEEDS_TURN_IN    = "B"
-
-                        -- Quest completed. Done for the day.
-                        -- "Checkmark" icon.
-DW.STATE_3_TURNED_IN        = "V"
-
-
--- RequirementList ------------------------------------------------------
+-- Requirement ----------------------------------------------------------
 --
--- Inventory list required to fill ONE quest.
+-- One required item for a quest, such as "3 Nirnroot" or "1 Rubedite Sword"
 --
-DW.RequirementList = {
+DW.Requirement = {
 }
-function DW.RequirementList:New()
-    local o = { ct      = 0
-              , item_id = 0
-              , name    = nil
-              , crafted = true -- true  = must be crafted
-                               -- false = extracted from bags
-                               --         (usually craft bag)
-              }
+function DW.Requirement:FromArgs(r_args)
+    local o =  { ct      = r_args[1]
+               , item_id = r_args[2]
+               , name    = r_args[3]
+               , crafted = true
+               }
+    if r_args.crafted ~= nil then
+        o.crafted = r_args.crafted
+    end
     setmetatable(o, self)
     self.__index = self
     return o
 end
 
-function DW.RequirementList:FromList(list)
-    o = self:New()
-    for i, e in ipairs(list) do
-        o[i].ct      = a[1]
-        o[i].item_id = a[2]
-        o[i].name    = a[3]
-        if a.crafted ~= nil then
-            o[i].crafted = a.crafted
-        end
+-- RequirementList ------------------------------------------------------
+--
+-- isa list of Requirement
+--
+-- Inventory list required to fill ONE quest.
+-- Usually 2-3 elements.
+--
+DW.RequirementList = {
+}
+function DW.RequirementList:FromList(rl)
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+    for i, r_args in ipairs(rl) do
+        table.insert(o, DW.Requirement:FromArgs(r_args))
     end
+    return o
 end
 
 -- RequirementSequence ----------------------------------------------
 --
--- A sequence of THREE quest's worth of inventory requirements. Used to
+-- isa list of RequirementList
+--
+-- A sequence of 3-5 quest's worth of inventory requirements. Used to
 -- scan through inventory to see how many days' worth of quests we can
 -- do before we will have to visit a crafting station.
 --
 DW.RequirementSequence = {
 }
-function DW.RequirementSequence:New(r1, r2, r3)
-    local o = {
-        list = {}
-              }
-    if r1 then list[1] = DW.RequirementList:FromList(r1) end
-    if r2 then list[2] = DW.RequirementList:FromList(r2) end
-    if r3 then list[3] = DW.RequirementList:FromList(r3) end
-
+function DW.RequirementSequence:FromList(rl_list)
+    local o = {}
+    for _, rl in ipairs(rl_list) do
+        local req = DW.RequirementList:FromList(rl)
+        table.insert(self, req)
+    end
     setmetatable(o, self)
     self.__index = self
     return o
@@ -94,37 +79,14 @@ function DW.RequirementSequence:Nth(n, today_n)
 end
 
 local REQSEQ_BLACKSMITH_10 = {
--- Day 1
--- Zhaksyr  2017-03-29 zig: D1
--- Zecorwyn 2017-03-29 zig: D1
--- Lilwen   2017-03-29 zig: D1
--- Simone   2017-03-29 zig: D1
--- Al       2017-03-30 zig: D2
--- Lilwen   2017-03-31 zig: D3
--- Hagnar   2017-03-31 zig: D3
   { { 1, 43562, "Rubedite Helm" }
   , { 1, 43535, "Rubedite Dagger" }
   , { 1, 43541, "Rubedite Pauldron" }
   }
-
--- Day 2
--- Hagnar   2017-03-29 zig: D1
--- Zhaksyr  2017-03-30 zig: D2
--- Zecorwyn 2017-03-30 zig: D2
--- Simone   2017-03-30 zig: D2
--- Al       2017-03-31 zig: D3
 , { { 1, 43534, "Rubedite Greatsword" }
   , { 1, 43538, "Rubedite Sabatons" }
   , { 1, 43539, "Rubedite Gauntlets" }
   }
-
--- Day 3
--- Al       2017-03-29 zig: D1
--- Lilwen   2017-03-30 zig: D2
--- Hagnar   2017-03-30 zig: D2
--- Zhaksyr  2017-03-31 zig: D3
--- Zecorwyn 2017-03-31 zig: D3
--- Simone   2017-03-31 zig: D3
 , { { 1, 43531, "Rubedite Sword" }
   , { 1, 43537, "Rubedite Cuirass" }
   , { 1, 43540, "Rubedite Greaves" }
@@ -132,35 +94,14 @@ local REQSEQ_BLACKSMITH_10 = {
 }
 
 local REQSEQ_CLOTHIER_10 = {
--- Day 1
--- Zhaksyr  2017-03-29 zig: D1
--- Zecorwyn 2017-03-29 zig: D1
--- Simone   2017-03-29 zig: D1
--- Al       2017-03-30 zig: D2
--- Lilwen   2017-03-31 zig: D3
--- Hagnar   2017-03-31 zig: D3
   { { 1, 43552, "Rubedo Leather Bracers" }
   , { 1, 43563, "Rubedo Leather Helmet" }
   , { 1, 43554, "Rubedo Leather Arm Cops" }
   }
--- Day 2
--- Lilwen   2017-03-29 zig: D1
--- Hagnar   2017-03-29 zig: D1
--- Zhaksyr  2017-03-30 zig: D2
--- Zecorwyn 2017-03-30 zig: D2
--- Simone   2017-03-30 zig: D2
--- Al       2017-03-31 zig: D3
 , { { 1, 43544, "Ancestor Silk Shoes" }
   , { 1, 43564, "Ancestor Silk Hat" }
   , { 1, 43548, "Ancestor Silk Sash" }
   }
--- Day 3
--- Al       2017-03-29 zig: D1
--- Lilwen   2017-03-30 zig: D2
--- Hagnar   2017-03-30 zig: D2
--- Zhaksyr  2017-03-31 zig: D3
--- Zecorwyn 2017-03-31 zig: D3
--- Simone   2017-03-31 zig: D3
 , { { 1, 43543, "Ancestor Silk Robe" }
   , { 1, 43546, "Ancestor Silk Breeches" }
   , { 1, 43547, "Ancestor Silk Epaulets" }
@@ -168,103 +109,40 @@ local REQSEQ_CLOTHIER_10 = {
 }
 
 local REQSEQ_WOODWORKING_10 = {
--- Day 1
--- Zhaksyr  2017-03-29 zig: D1
--- Zecorwyn 2017-03-29 zig: D1
--- Simone   2017-03-29 zig: D1
--- Al       2017-03-30 zig: D2
--- Lilwen   2017-03-31 zig: D3
--- Hagnar   2017-03-31 zig: D3
   { { 2, 43549, "Ruby Ash Bow" }
   , { 1, 43556, "Ruby Ash Shield" }
   }
--- Day 2
--- Lilwen   2017-03-29 zig: D1
--- Hagnar   2017-03-29 zig: D1
--- Zhaksyr  2017-03-30 zig: D2
--- Simone   2017-03-30 zig: D2
--- Al       2017-03-31 zig: D3
 , { { 1, 43557, "Ruby Ash Inferno Staff" }
   , { 1, 43558, "Ruby Ash Ice Staff" }
   , { 1, 43559, "Ruby Ash Lightning Staff" }
   }
--- Day 3
--- Al       2017-03-29 zig: D1
--- Lilwen   2017-03-30 zig: D2
--- Hagnar   2017-03-30 zig: D2
--- Zhaksyr  2017-03-31 zig: D3
--- Zecorwyn 2017-03-31 zig: D3
--- Simone   2017-03-31 zig: D3
 , { { 2, 43560, "Ruby Ash Restoration Staff" }
   , { 1, 43556, "Ruby Ash Shield" }
   }
 }
 
 local REQSEQ_ALCHEMY_10 = {
--- Day 1
--- Zhaksyr  2017-03-29 zig: D1
--- Lilwen   2017-03-29 zig: D1
--- Zecorwyn 2017-03-31 zig: D3
--- Al       2017-03-31 zig: D3
--- Hagnar   2017-03-31 zig: D3
   { { 1, 54340, "Essence of Magicka" }
   , { 3, 30165, "Nirnroot", crafted = false }
   }
--- Day 2
--- Simone   2017-03-29 zig: D1
--- Zhaksyr  2017-03-30 zig: D2
--- Lilwen   2017-03-30 zig: D2
 , { { 1, 54341, "Essence of Stamina" }
   , { 3, 30165, "Nirnroot", crafted = false }
   }
--- Day 3
--- Zecorwyn 2017-03-29 zig: D1
--- Al       2017-03-29 zig: D1
--- Hagnar   2017-03-29 zig: D1
--- Simone   2017-03-30 zig: D2
--- Zhaksyr  2017-03-31 zig: D3
--- Lilwen   2017-03-31 zig: D3
 , { { 1, 44812, "Essence of Ravage Health" }
   , { 3, 64501, "Lorkhan's Tears", crafted = false }
   }
--- Day 4 Zecorwyn
--- Al       2017-03-30 zig: D2
--- Zecorwyn 2017-03-30 zig: D2
--- Hagnar   2017-03-30 zig: D2
--- Simone   2017-03-31 zig: D3
 , { { 1, 54339, "Essence of Health" }
   , { 3, 30165, "Nirnroot", crafted = false }
   }
 }
 
 local REQSEQ_ENCHANTING_10 = {
--- Day 1
--- Zhaksyr  2017-03-29 zig: D1
--- Zecorwyn 2017-03-29 zig: D1
--- Simone   2017-03-29 zig: D1
--- Al       2017-03-30 zig: D2
--- Lilwen   2017-03-31 zig: D3
--- Hagnar   2017-03-31 zig: D3
   { { 1, 26588, "Superb Glyph of Stamina" }
   , { 1, 45850, "Ta" , crafted = false }
   }
--- Day 2
--- Lilwen   2017-03-29 zig: D1
--- Hagnar   2017-03-29 zig: D1
--- Zhaksyr  2017-03-30 zig: D2
--- Zecorwyn 2017-03-30 zig: D2
--- Simone   2017-03-30 zig: D2
--- Al       2017-03-31 zig: D3
 , { { 1, 26580, "Superb Glyph of Health" }
   , { 1, 64508, "Jehade", crafted = false }
   }
--- Day 3
--- Al       2017-03-29 zig: D1
--- Lilwen   2017-03-30 zig: D2
--- Hagnar   2017-03-30 zig: D2
--- Zhaksyr  2017-03-31 zig: D3
--- Zecorwyn 2017-03-31 zig: D3
--- Simone   2017-03-31 zig: D3
 , { { 1, 26582, "Superb Glyph of Magicka" }
   , { 1, 45831, "Oko", crafted = false }
   }
@@ -272,18 +150,12 @@ local REQSEQ_ENCHANTING_10 = {
 
 -- Level 20-25 recipes, Hagnar
 local REQSEQ_PROVISIONING_2_EP = {
--- Day 1
--- Hagnar  2017-03-29 zig:
   { { 1, 28394, "Battaglir Chowder" }
   , { 1, 28417, "Eltheric Hooch" }
   }
--- Day 2
--- Hagnar   2017-03-30 zig: D2
 , { { 1, 28398, "Venison Pasty" }
   , { 1, 28421, "Honey Rye" }
   }
--- Day 3
--- Hagnar   2017-03-31 zig: D3
 , { { 1, 33552, "Redoran Peppered Melon" }
   , { 1, 28457, "Bitterlemon Tea" }
   }
@@ -291,18 +163,12 @@ local REQSEQ_PROVISIONING_2_EP = {
 
 -- Level 30-35 recipes, Zecorwyn.
 local REQSEQ_PROVISIONING_3_AD = {
--- Day 1
--- Zecorwyn 2017-03-29 zig: D1
   { { 1, 33789, "Cyrodilic Pumpkin Fritters" }
   , { 1, 33684, "Spiceberry Chai" }
   }
--- Day 2
--- Zecorwyn 2017-03-30 zig: D2
 , { { 1, 33514, "Chorrol Corn on the Cob" }
   , { 1, 33636, "Spiced Matze" }
   }
--- Day 3
--- Zecorwyn 2017-03-31 zig: D3
 , { { 1, 33520, "Elinhir Roast Antelope" }
   , { 1, 33642, "Sorry, Honey Lager" }
   }
@@ -310,18 +176,12 @@ local REQSEQ_PROVISIONING_3_AD = {
 
 -- Level 40-45 recipes, Al
 local REQSEQ_PROVISIONING_4_AD = {
--- Day 1
--- Al       2017-03-29 zig: D1
   { { 1, 33903, "Mammoth Snout Pie" }
   , { 1, 28473, "Two-Zephyr Tea" }
   }
--- Day 2
--- Al       2017-03-30 zig: D2
 , { { 1, 33909, "Skyrim Jazbay Crostata" }
   , { 1, 28513, "Blue Road Marathon" }
   }
--- Day 3
--- Al       2017-03-31 zig: D3
 , { { 1, 33897, "Cyrodilic Cornbread" }
   , { 1, 28433, "Gods-Blind-Me" }
   }
@@ -329,51 +189,60 @@ local REQSEQ_PROVISIONING_4_AD = {
 
 -- CP10-50 recipes Simone
 local REQSEQ_PROVISIONING_5_DC = {
--- Day 1
--- Simone 2017-03-29 zig:
   { { 1, 43094, "Orcrest Garlic Apple Jelly" }
   , { 1, 28444, "Grandpa's Bedtime Tonic" }
   }
--- Day 2
--- Simone   2017-03-30 zig: D2
 , { { 1, 32160, "West Weald Corn Chowder" }
   , { 1, 28402, "Comely Wench Whisky" }
   }
---Day 3
--- Simone   2017-03-31 zig: D3
 , { { 1, 43088, "Millet-Stuffed Pork Loin" }
   , { 1, 33602, "Aetherial Tea" }
   }
 }
 
--- Max level provisioning: Zhaksyr and Lilwen
+-- Max level provisioning: Zhaksyr
 -- CP100-150 recipes
-local REQSEQ_PROVISIONING_6 = {
--- Day 1
+local REQSEQ_PROVISIONING_6_ZH = {
 -- Zhaksyr  2017-03-29 zig: D1
--- Lilwen   2017-03-31 zig: D3
   { { 1, 68236, "Firsthold Fruit and Cheese Plate" }
   , { 1, 68260, "Muthsera's Remorse" }
   }
--- Day 2 Zh
--- Zhaksyr  2017-03-30 zig: D2
+-- Zhaksyr  2017-03-30 zig: D2 D5
 , { { 1, 43124, "Pickled Carrot Slurry" }
   , { 1, 33652, "Arenthian Brandy" }
   }
--- Day 2
--- Lilwen   2017-03-29 zig: D1
-, { { 1, 68239, "Hearty Garlic Corn Chowder" }
-  , { 1, 68257, "Markarth Mead" }
-  }
--- Day 3
 -- Zhaksyr  2017-03-31 zig: D3
 , { { 1, 68235, "Lilmoth Garlic Hagfish" }
   , { 1, 68263, "Hagraven's Tonic" }
   }
--- Day 2
+-- Zhaksyr  2017-04-01 zig: D4
+, { { 1, 43154, "Fresh Apples and Eidar Cheese" }
+  , { 1, 28482, "Khenarthi's Wings Chai" }
+  }
+-- Zhaksyr  2017-04-03 zig:    D6
+, { { 1, 43142, "Argonian Saddle-Cured Rabbit" }
+  , { 1, 33698, "Sipping Imga Tonic" }
+  }
+}
+
+-- Max level provisioning: Lilwen
+-- CP100-150 recipes
+local REQSEQ_PROVISIONING_6_LI = {
+-- Lilwen   2017-03-29 zig: D1 D4
+, { { 1, 68239, "Hearty Garlic Corn Chowder" }
+  , { 1, 68257, "Markarth Mead" }
+  }
 -- Lilwen   2017-03-30 zig: D2
 , { { 1, 43142, "Argonian Saddle-Cured Rabbit" }
   , { 1, 33698, "Sipping Imga Tonic" }
+  }
+-- Lilwen   2017-03-31 zig: D3 D6
+  { { 1, 68236, "Firsthold Fruit and Cheese Plate" }
+  , { 1, 68260, "Muthsera's Remorse" }
+  }
+-- Lilwen   2017-04-02 zig:    D5
+, { { 1, 68235, "Lilmoth Garlic Hagfish" }
+  , { 1, 68263, "Hagraven's Tonic" }
   }
 }
 
@@ -402,55 +271,96 @@ DW.CHAR_HAGNAR   = "Hagnar the Slender"
 
 -- Character-specific requirement sequences.
 DW.REQSEQ = {
-  [DW.CHAR_ZHAKSYR] = {
+  [DW.CHAR_ZHAKSYR] = DW.RequirementSequence:FromList({
       [CRAFTING_TYPE_BLACKSMITHING] = REQSEQ_BLACKSMITH_10
     , [CRAFTING_TYPE_CLOTHIER]      = REQSEQ_CLOTHIER_10
     , [CRAFTING_TYPE_WOODWORKING]   = REQSEQ_WOODWORKING_10
     , [CRAFTING_TYPE_ENCHANTING]    = REQSEQ_ENCHANTING_10
     , [CRAFTING_TYPE_ALCHEMY]       = REQSEQ_ALCHEMY_10
-    , [CRAFTING_TYPE_PROVISIONING]  = REQSEQ_PROVISIONING_10
-    }
-, [DW.CHAR_ZECORWYN] = {
+    , [CRAFTING_TYPE_PROVISIONING]  = REQSEQ_PROVISIONING_6_ZH
+    })
+, [DW.CHAR_ZECORWYN] = DW.RequirementSequence:FromList({
       [CRAFTING_TYPE_BLACKSMITHING] = REQSEQ_BLACKSMITH_10
     , [CRAFTING_TYPE_CLOTHIER]      = REQSEQ_CLOTHIER_10
     , [CRAFTING_TYPE_WOODWORKING]   = REQSEQ_WOODWORKING_10
     , [CRAFTING_TYPE_ENCHANTING]    = REQSEQ_WOODWORKING_10
     , [CRAFTING_TYPE_ALCHEMY]       = REQSEQ_ALCHEMY_10
     , [CRAFTING_TYPE_PROVISIONING]  = REQSEQ_PROVISIONING_3_AD
-    }
-, [DW.CHAR_LILWEN] = {
+    })
+, [DW.CHAR_LILWEN] = DW.RequirementSequence:FromList({
       [CRAFTING_TYPE_BLACKSMITHING] = REQSEQ_BLACKSMITH_10
     , [CRAFTING_TYPE_CLOTHIER]      = REQSEQ_CLOTHIER_10
     , [CRAFTING_TYPE_WOODWORKING]   = REQSEQ_WOODWORKING_10
     , [CRAFTING_TYPE_ENCHANTING]    = REQSEQ_WOODWORKING_10
     , [CRAFTING_TYPE_ALCHEMY]       = REQSEQ_ALCHEMY_10
-    , [CRAFTING_TYPE_PROVISIONING]  = REQSEQ_PROVISIONING_10
-    }
-, [DW.CHAR_AL] = {
+    , [CRAFTING_TYPE_PROVISIONING]  = REQSEQ_PROVISIONING_6_LI
+    })
+, [DW.CHAR_AL] = DW.RequirementSequence:FromList({
       [CRAFTING_TYPE_BLACKSMITHING] = REQSEQ_BLACKSMITH_10
     , [CRAFTING_TYPE_CLOTHIER]      = REQSEQ_CLOTHIER_10
     , [CRAFTING_TYPE_WOODWORKING]   = REQSEQ_WOODWORKING_10
     , [CRAFTING_TYPE_ENCHANTING]    = REQSEQ_WOODWORKING_10
     , [CRAFTING_TYPE_ALCHEMY]       = REQSEQ_ALCHEMY_10
     , [CRAFTING_TYPE_PROVISIONING]  = REQSEQ_PROVISIONING_4_AD
-    }
-, [DW.CHAR_SIMONE] = {
+    })
+, [DW.CHAR_SIMONE] = DW.RequirementSequence:FromList({
       [CRAFTING_TYPE_BLACKSMITHING] = REQSEQ_BLACKSMITH_10
     , [CRAFTING_TYPE_CLOTHIER]      = REQSEQ_CLOTHIER_10
     , [CRAFTING_TYPE_WOODWORKING]   = REQSEQ_WOODWORKING_10
     , [CRAFTING_TYPE_ENCHANTING]    = REQSEQ_WOODWORKING_10
     , [CRAFTING_TYPE_ALCHEMY]       = REQSEQ_ALCHEMY_10
     , [CRAFTING_TYPE_PROVISIONING]  = REQSEQ_PROVISIONING_5_DC
-    }
-, [DW.CHAR_HAGNAR] = {
+    })
+, [DW.CHAR_HAGNAR] = DW.RequirementSequence:FromList({
       [CRAFTING_TYPE_BLACKSMITHING] = REQSEQ_BLACKSMITH_10
     , [CRAFTING_TYPE_CLOTHIER]      = REQSEQ_CLOTHIER_10
     , [CRAFTING_TYPE_WOODWORKING]   = REQSEQ_WOODWORKING_10
     , [CRAFTING_TYPE_ENCHANTING]    = REQSEQ_WOODWORKING_10
     , [CRAFTING_TYPE_ALCHEMY]       = REQSEQ_ALCHEMY_10
     , [CRAFTING_TYPE_PROVISIONING]  = REQSEQ_PROVISIONING_2_EP
-    }
+    })
 }
+
+-- Quest states --------------------------------------------------------------
+
+                        -- Not started
+                        -- Need to visit a billboard to acquire quest.
+                        -- "X" icon.
+DW.STATE_0_NEEDS_ACQUIRE    = "X"
+
+                        -- Acquired, but at least one item needs to be
+                        -- crafted before turnining in.
+                        -- Need to visit a crafting station to
+                        -- make things.
+                        -- "Anvil" icon.
+DW.STATE_1_NEEDS_CRAFTING   = "A"
+
+                        -- Crafting of all items completed.
+                        -- Need to visit turn-in station.
+                        -- "Bag" icon
+DW.STATE_2_NEEDS_TURN_IN    = "B"
+
+                        -- Quest completed. Done for the day.
+                        -- "Checkmark" icon.
+DW.STATE_3_TURNED_IN        = "V"
+
+
+-- QuestStatus --------------------------------------------------------------
+--
+-- How far along is this quest? When do we thing this quest was first acquired?
+DW.QuestStatus = {
+}
+function DW.QuestStatus:New()
+    local o = { status      = DW.STATE_0_NEEDS_ACQUIRE
+              , acquired_ts = 0  -- either 11pm yesterday or
+                                 -- when we got event for acquiring
+              , day_offset  = 0  -- into associated RequirementSequence
+              }
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
 
 -- CharData ==================================================================
 --
@@ -475,7 +385,7 @@ local CharData = {
                         -- XXX discard "quest completed" edges that occur
                         -- XXX older than 11pm last night.
 
-,   day_offsets = {}    -- index = CRAFTING_TYPE_XXX
+,   curr_day_offset = {} -- index = CRAFTING_TYPE_XXX
                         -- value = number 0-2 offset into corresponding
                         --         reqseq sequence where next quest
                         --         is/will be.
@@ -488,6 +398,12 @@ local CharData = {
     InventoryList(craft_type)
 --]]
 }
+function CharData:New()
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
 
 
 
