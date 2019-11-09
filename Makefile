0d34726d3548a746a5f9b55b747f68b8c14c27e7
Zig Zichterman <github@ziggr.com>
2019-11-09 14:48:56 -0800

    logging tools

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: put putall
+.PHONY: get put getpts log
 
 put:
 	rsync -vrt --delete --exclude=.git . /Volumes/Elder\ Scrolls\ Online/live/AddOns/ZZDailyWrits
@@ -11,3 +11,5 @@ getpts:
 	cp -f /Volumes/Elder\ Scrolls\ Online/pts/SavedVariables/ZZDailyWrits.lua data/
 	-cp -f /Volumes/Elder\ Scrolls\ Online/pts/SavedVariables/LibDebugLogger.lua data/
 
+log:
+	lua tool/log_to_text.lua > data/log.txt
---
+++ tool/log_to_text.lua
@@ -0,0 +1,31 @@
+--[[
+Convert LibDebugLogger saved_vars to a text dump of just HomeStationMarker
+log lines.
+--]]
+
+dofile("data/LibDebugLogger.lua")
+
+NAM = "ZZDailyWrits"
+LDB = "LibDebugLogger"
+INT = "Initializing..."
+
+function StartsWith(longer, prefix)
+   return longer:sub(1, #prefix) == prefix
+end
+
+function Row6(row6)
+    if type(row6) ~= "table" then return tostring(row6) end
+    return table.concat(row6,"")
+end
+
+for i,row in ipairs(LibDebugLoggerLog) do
+    if row[5] == NAM
+        or row[5] == "UI"
+        then
+        print(row[4].." "..Row6(row[6]))
+    elseif row[5] == LDB and StartsWith(Row6(row[6]), INT) then
+        print("")
+        print(row[2])
+    end
+end
+
